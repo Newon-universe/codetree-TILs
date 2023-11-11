7f2aa5c442ee66a1b5aa141313b2f346029a822d
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-11 19:22:05 +0900

    Title: 일렬로 서있는 소 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 231111/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.swift
+++ 231111/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.swift
@@ -5,7 +5,7 @@ var result = 0
 for i in 0 ..< array.count - 2 {
     for j in i + 1 ..< array.count - 1 {
         for k in j + 1 ..< array.count {
-            if array[i] < array[j] && array[j] < array[k] {
+            if array[i] <= array[j] && array[j] <= array[k] {
                 result += 1
             }
         }
