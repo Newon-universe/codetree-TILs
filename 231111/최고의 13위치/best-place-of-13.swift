700808cb430adcd50419b46727b6722a3499b171
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-11 19:28:57 +0900

    Title: 최고의 13위치, Time: 18ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 231111/최고의 13위치/best-place-of-13.swift
@@ -0,0 +1,22 @@
+let n = Int(readLine()!)!
+var graph = [[Int]]()
+var result = 0
+
+for _ in 0 ..< 3 {
+    graph.append(readLine()!.split { $0 == " " }.map { Int($0)! })
+}
+
+for i in 0 ..< n {
+    for j in 0 ..< n {
+
+        guard j + 2 < n else { continue }
+        var temp = 0
+
+        for k in j ... j + 2 {
+            if graph[i][k] == 1 { temp += 1 }
+        }
+        result = max(temp, result)
+    }
+}
+
+print(result)
