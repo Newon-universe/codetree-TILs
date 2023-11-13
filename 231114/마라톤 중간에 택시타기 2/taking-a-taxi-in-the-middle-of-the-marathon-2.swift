7a7ef61d2d1b8bf8b006514b530f32bfd82c6a5d
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-14 00:56:36 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 231114/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+struct Coordinate: Hashable {
+    var y: Int
+    var x: Int
+    init(_ y: Int, _ x: Int) {
+        self.y = y
+        self.x = x
+    }
+}
+
+let n = Int(readLine()!)!
+var checkPoints = [Coordinate]()
+var result = Int.max
+
+for _ in 0 ..< n {
+    let raw = readLine()!.split { $0 == " " }.map { Int($0)! }
+    checkPoints.append(Coordinate(raw[1], raw[0]))
+}
+
+for i in 1 ..< n - 1 {
+    var temp = Coordinate(0, 0)
+    var distance = 0
+
+    for j in 1 ..< n {
+        if i == j { continue }
+
+        temp.x = abs(temp.x - checkPoints[j].x)
+        temp.y = abs(temp.y - checkPoints[j].y)
+        distance += temp.x + temp.y
+    }
+
+    result = min(distance, result)
+}
+
+print(result)
