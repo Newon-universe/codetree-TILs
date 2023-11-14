b13cf24276ae25426058504f0b789e9027c6b9c5
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-15 00:36:53 +0900

    Title: G or H 3, Time: 22ms, Memory: 0MB, Status: Passed - CodeTree

--- 231115/G or H 3/G-or-H-3.swift
+++ 231115/G or H 3/G-or-H-3.swift
@@ -2,15 +2,15 @@ let raw = readLine()!.split { $0 == " " }.map { Int($0)! }
 let (N, K) = (raw[0], raw[1])
 var result = 0
 
-var board = [Int](repeating: 0, count: 101)
+var board = [Int](repeating: 0, count: 10001)
 for _ in 0 ..< N {
     let value = readLine()!.split { $0 == " " }
-    let index = Int(value[0])! - 1
+    let index = Int(value[0])!
     let score = value[1] == "G" ? 1 : 2
     board[index] = score
 }
 
-for i in 0 ... N - K {
+for i in 1 ..< 10001 - K {
     result = max(board[i ... i + K].reduce(0, +), result)
 }
 
