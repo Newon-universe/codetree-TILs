96eb9de4aca8dd97b9eae078388f3aa5e13d45be
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-15 00:16:20 +0900

    Title: 구간 중 최대 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231115/구간 중 최대 합/max-sum-of-subarray.swift
@@ -0,0 +1,10 @@
+let raw = readLine()!.split { $0 == " " }.map { Int($0)! }
+let (N, K) = (raw[0], raw[1])
+let array = readLine()!.split { $0 == " " }.map { Int($0)! }
+var result = 0
+
+for i in 0 ..< N - K {
+    result = max(array[i ..< i + K].reduce(0, +), result)
+}
+
+print(result)
