453ce260443e3a868c37b60bbc55dca2960e59da
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-14 11:55:47 +0900

    Title: 씨 오 더블유 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 231114/씨 오 더블유 2/c-o-w-2.swift
+++ 231114/씨 오 더블유 2/c-o-w-2.swift
@@ -1,14 +1,14 @@
-func dfs(_ depth: Int) {
+func dfs(_ depth: Int, _ index: Int) {
     if depth == 3 {
         result += 1
         return
     }
 
-    for i in 0 ..< n {
+    for i in index ..< n {
         switch depth {
-        case 0: if array[i] == "C" { dfs(depth + 1) }
-        case 1: if array[i] == "O" { dfs(depth + 1) }
-        case 2: if array[i] == "W" { dfs(depth + 1) }
+        case 0: if array[i] == "C" { dfs(depth + 1, i + 1) }
+        case 1: if array[i] == "O" { dfs(depth + 1, i + 1) }
+        case 2: if array[i] == "W" { dfs(depth + 1, i + 1) }
         default: break
         }
     }
@@ -19,5 +19,5 @@ let n = Int(readLine()!)!
 let array = Array(readLine()!).map { String($0) }
 var result = 0
 
-dfs(0)
+dfs(0, 0)
 print(result)
