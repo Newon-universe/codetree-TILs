68ea4f063c153b840107501c4781261baf9d27c7
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-14 20:34:36 +0900

    Title: 최고의 13위치 2, Time: 22ms, Memory: 3MB, Status: Passed - CodeTree

--- 231114/최고의 13위치 2/best-place-of-13-2.swift
+++ 231114/최고의 13위치 2/best-place-of-13-2.swift
@@ -1,4 +1,4 @@
-struct Coordinate {
+struct Coordinate: Equatable {
     var y: Int
     var x: Int
     init(_ y: Int, _ x: Int) {
@@ -7,8 +7,24 @@ struct Coordinate {
     }
 }
 
+func combination(_ depth: Int, _ trace: [Coordinate]) {
+    if depth == 2 {
+        applicant.append(trace)
+        return
+    }
+
+    for i in (trace.last?.y ?? 0) ..< n {
+        for j in (trace.last?.x ?? 0) ..< n {
+            if trace.last?.y == i && (trace.last?.x ?? n) ... (trace.last?.x ?? n) + 2 ~= j { continue }
+            
+            combination(depth + 1, trace + [Coordinate(i, j)])
+        }
+    }
+}
+
 func find(_ start: Coordinate) -> Int {
     var count = 0
+    guard start.x + 2 < n else { return 0 }
     graph[start.y][start.x ... start.x + 2].map { if $0 == 1 { count += 1 } }
     return count
 }
@@ -16,28 +32,18 @@ func find(_ start: Coordinate) -> Int {
 
 let n = Int(readLine()!)!
 var graph = [[Int]]()
+var applicant = [[Coordinate]]()
 var result = 0
 
 for _ in 0 ..< n {
     graph.append(readLine()!.split { $0 == " " }.map { Int($0)! })
 }
 
-for i in 0 ..< n {
+combination(0, [])
 
-    for j in 0 ..< n - 2 {
-        let temp = find(Coordinate(i, j))
-
-        if j == n - 3 && i < n - 1{
-            let value = find(Coordinate(i + 1, 0))
-            result = max(temp + value, result)
-        } else {
-            for k in j + 1 ..< n - 2 {
-                let value = find(Coordinate(i, k))
-                result = max(temp + value, result)
-            }
-        }
-    }
-    
+for candi in applicant {
+    var temp = find(candi[0]) + find(candi[1])
+    result = max(temp, result)
 }
 
 print(result)
