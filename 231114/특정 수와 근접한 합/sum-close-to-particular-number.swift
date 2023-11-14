ce621da8f910639aa21ffb52f44bd9284b54d225
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-14 20:51:47 +0900

    Title: 특정 수와 근접한 합, Time: 1014ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 231114/특정 수와 근접한 합/sum-close-to-particular-number.swift
+++ 231114/특정 수와 근접한 합/sum-close-to-particular-number.swift
@@ -1,10 +1,10 @@
-func combination(_ depth: Int, _ trace: [Int], _ index: Int) {
+func combination(_ depth: Int, _ now: Int, _ index: Int) {
     if depth == N - 2 {
-        result = min(abs(trace.reduce(0, +) - S), result)
+        result = min(abs(now - S), result)
     }
 
     for i in index ..< N {
-        combination(depth + 1, trace + [array[i]], i + 1)
+        combination(depth + 1, now + array[i], i + 1)
     }
 }
 
@@ -14,5 +14,5 @@ let array = readLine()!.split { $0 == " " }.map { Int($0)! }
 
 var result = S
 
-combination(0, [], 0)
+combination(0, 0, 0)
 print(result)
