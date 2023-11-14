8a51294eac083b2838647cc4e32eabe487ee2eff
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-15 00:17:03 +0900

    Title: 구간 중 최대 합, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 231115/구간 중 최대 합/max-sum-of-subarray.swift
+++ 231115/구간 중 최대 합/max-sum-of-subarray.swift
@@ -3,7 +3,7 @@ let (N, K) = (raw[0], raw[1])
 let array = readLine()!.split { $0 == " " }.map { Int($0)! }
 var result = 0
 
-for i in 0 ..< N - K {
+for i in 0 ... N - K {
     result = max(array[i ..< i + K].reduce(0, +), result)
 }
 
