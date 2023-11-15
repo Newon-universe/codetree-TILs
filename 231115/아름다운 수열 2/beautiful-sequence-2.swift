9b51884606cc11483e8bfbb971ab16427d9eb542
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-15 10:14:03 +0900

    Title: 아름다운 수열 2, Time: 18ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 231115/아름다운 수열 2/beautiful-sequence-2.swift
+++ 231115/아름다운 수열 2/beautiful-sequence-2.swift
@@ -1,31 +1,26 @@
-func permutation(_ depth: Int, _ trace: [Int]) {
-    if depth == M {
-        beautifulB.append(trace)
-        return
-    }
+func solve() {
+    for i in 0 ... N - M {
+        let standard = A[i ..< i + M]
+        var visited = [Bool](repeating: false, count: M)
 
-    for i in 0 ..< M {
-        guard !visited[i] else { continue }
-        visited[i] = true
-        permutation(depth + 1, trace + [B[i]])
-        visited[i] = false
+        standard.forEach {
+            for j in 0 ..< M {
+                guard !visited[j] else { continue }
+                if B[j] == $0 { visited[j] = true }
+            }
+        }
+
+        if visited.allSatisfy { $0 == true } { result += 1 }
     }
 }
+
 
 let raw = readLine()!.split { $0 == " " }.map { Int($0)! }
 let (N, M) = (raw[0], raw[1])
 let A = readLine()!.split { $0 == " " }.map { Int($0)! }
 let B = readLine()!.split { $0 == " " }.map { Int($0)! }
-var beautifulB = [[Int]]()
-var visited = [Bool](repeating: false, count: M)
 var result = 0
 
-permutation(0, [])
-
-for i in 0 ... N - M {
-    for element in beautifulB {
-        if Array(A[i ..< i + M]) == element { result += 1; break }
-    }
-}
+solve()
 
 print(result)
