d41339f7cea9c9adbc0a976f88c2d85b6e7b5b6c
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-15 14:17:41 +0900

    Title: 바구니 안의 사탕 2, Time: 18ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 231115/바구니 안의 사탕 2/candy-in-the-basket-2.swift
+++ 231115/바구니 안의 사탕 2/candy-in-the-basket-2.swift
@@ -5,13 +5,13 @@ var result = 0
 
 for _ in 0 ..< N {
     let basket = readLine()!.split { $0 == " " }.map { Int($0)! }
-    board[basket[1]] = basket[0]
+    board[basket[1]] += basket[0]
 }
 
 for i in K ... 100 - K {
     var temp = 0
     for j in i - K ... i + K { temp += board[j] }
-    
+
     result = max(temp, result)
 }
 
