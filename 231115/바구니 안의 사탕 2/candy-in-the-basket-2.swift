47905700382f340d86a87dcf28b29aedd7f1e595
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-15 14:16:36 +0900

    Title: 바구니 안의 사탕 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231115/바구니 안의 사탕 2/candy-in-the-basket-2.swift
@@ -0,0 +1,18 @@
+let raw = readLine()!.split { $0 == " " }.map { Int($0)! }
+let (N, K) = (raw[0], raw[1])
+var board = [Int](repeating: 0, count: 101)
+var result = 0
+
+for _ in 0 ..< N {
+    let basket = readLine()!.split { $0 == " " }.map { Int($0)! }
+    board[basket[1]] = basket[0]
+}
+
+for i in K ... 100 - K {
+    var temp = 0
+    for j in i - K ... i + K { temp += board[j] }
+    
+    result = max(temp, result)
+}
+
+print(result)
