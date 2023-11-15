4d7f4ccce3abd01f3c9922e0223fd2b59c00e74b
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-15 20:54:11 +0900

    Title: 흥미로운 숫자 2, Time: 618ms, Memory: 0MB, Status: Passed - CodeTree

--- 231115/흥미로운 숫자 2/interesting-numbers-2.swift
+++ 231115/흥미로운 숫자 2/interesting-numbers-2.swift
@@ -4,10 +4,14 @@ var result = 0
 
 for number in X ... Y {
     let value = Array(String(number))
-    var check = Set<Character>()
-    for element in value { check.insert(element) }
+    guard !value.allSatisfy({ $0 == value[0] }) else { continue }
+
+    var check = [Character : Int]()
+    for element in value {
+        check[element, default: 0] += 1
+    }
     
-    if check.count == 2 { result += 1 }
+    if check.count == 2 && check.values.filter { $0 == 1 }.count == 1 { result += 1 }
 }
 
 print(result)
