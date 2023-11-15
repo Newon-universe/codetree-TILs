2b181667db2fdab492d0462250c314c74114fca7
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-15 09:39:40 +0900

    Title: 특정 구간의 원소 평균값, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 231115/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.swift
+++ 231115/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.swift
@@ -1,16 +1,12 @@
-func find(_ depth: Int, _ trace: [Int], _ index: Int, _ target: Int) {
-    if depth == target {
-        let value = Double(trace.reduce(0, +)) / Double(target)
+func find(_ target: Int) {
+    for i in 0 ..< N {
+        guard i + target < N else { break }
+        let trace = array[i ... i + target]
+        let value = Double(trace.reduce(0, +)) / Double(target + 1)
+
         for element in trace {
             if Double(element) == value { result += 1; break }
         }
-
-        return
-    }
-
-    for i in index ..< N {
-        if i > 0 && array[i - 1] != trace.last { continue }
-        find(depth + 1, trace + [array[i]], i + 1, target)
     }
 }
 
@@ -19,8 +15,8 @@ let N = Int(readLine()!)!
 let array = readLine()!.split { $0 == " " }.map { Int($0)! }
 var result = N
 
-for i in 2 ... N {
-    find(0, [], 0, i)
+for i in 1 ..< N {
+    find(i)
 }
 
 print(result)
