f0aad5619fd6c6e6b98fdc21064199652b624e82
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-15 10:00:16 +0900

    Title: 아름다운 수열 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231115/아름다운 수열 2/beautiful-sequence-2.swift
@@ -0,0 +1,31 @@
+func permutation(_ depth: Int, _ trace: [Int]) {
+    if depth == M {
+        beautifulB.append(trace)
+        return
+    }
+
+    for i in 0 ..< M {
+        guard !visited[i] else { continue }
+        visited[i] = true
+        permutation(depth + 1, trace + [B[i]])
+        visited[i] = false
+    }
+}
+
+let raw = readLine()!.split { $0 == " " }.map { Int($0)! }
+let (N, M) = (raw[0], raw[1])
+let A = readLine()!.split { $0 == " " }.map { Int($0)! }
+let B = readLine()!.split { $0 == " " }.map { Int($0)! }
+var beautifulB = [[Int]]()
+var visited = [Bool](repeating: false, count: M)
+var result = 0
+
+permutation(0, [])
+
+for i in 0 ... N - M {
+    for element in beautifulB {
+        if Array(A[i ..< i + M]) == element { result += 1 }
+    }
+}
+
+print(result)
