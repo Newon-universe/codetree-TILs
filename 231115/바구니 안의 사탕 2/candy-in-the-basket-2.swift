ae24720d2094c7f043f080e284c4bc787dbf782f
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-15 14:24:00 +0900

    Title: 바구니 안의 사탕 2, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 231115/바구니 안의 사탕 2/candy-in-the-basket-2.swift
+++ 231115/바구니 안의 사탕 2/candy-in-the-basket-2.swift
@@ -8,11 +8,16 @@ for _ in 0 ..< N {
     board[basket[1]] += basket[0]
 }
 
-for i in K ... 100 - K {
-    var temp = 0
-    for j in i - K ... i + K { temp += board[j] }
-
-    result = max(temp, result)
+if K <= 50 {
+    for i in K ... 100 - K {
+        var temp = 0
+        for j in i - K ... i + K { temp += board[j] }
+        
+        result = max(temp, result)
+    }
+} else {
+    result = board.reduce(0, +)
 }
+
 
 print(result)
