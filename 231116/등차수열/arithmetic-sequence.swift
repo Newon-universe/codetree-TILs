cae801f18a29924252b50cb78849d72dd7f87b0c
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-16 13:42:44 +0900

    Title: 등차수열, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 231116/등차수열/arithmetic-sequence.swift
+++ 231116/등차수열/arithmetic-sequence.swift
@@ -3,13 +3,13 @@ import Foundation
 let n = Int(readLine()!)!
 
 let array = readLine()!.split { $0 == " " }.map { Int($0)! }
-var result = 0
+var result = [Double: Int]()
 
 for i in 0 ..< n - 1 {
     for j in i + 1 ..< n {
         let mid = (Double(array[i]) + Double(array[j])) / Double(2)
-        if mid.truncatingRemainder(dividingBy: 1.0) == 0 { result += 1 }
+        if abs(Double(array[i]) - mid) == abs(Double(array[j]) - mid) { result[mid, default:0] += 1 }
     }
 }
 
-print(result)
+print(result.values.max() ?? 0)
