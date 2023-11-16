76fc7a4b7f6f37aed0fb5837daf9d0acb6382147
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-16 21:23:28 +0900

    Title: 진수 to 진수, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231116/진수 to 진수/transformation-of-number-system.swift
@@ -0,0 +1,26 @@
+func makePow(_ raw: Int, _ radix: Int) -> Int {
+    var result = 1
+    for _ in 0 ..< raw { result *= radix }
+    return result
+}
+
+func makeB(_ raw: Int) -> String {
+    var raw = raw
+    var result = ""
+
+    while raw > 0 {
+        result = String(raw % b) + result
+        raw /= b
+    }
+
+    return result
+}
+
+let raw = readLine()!.split { $0 == " " }.map { Int($0)! }
+let (a, b) = (raw[0], raw[1])
+let n = Array(readLine()!).map { Int(String($0))! }
+var result = 0
+
+for i in 0 ..< n.count { result += makePow(i, a) }
+
+print(makeB(result))
