17b25d5f26db5e76634565ba7447b42ec4c6ff18
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-17 11:11:50 +0900

    Title: 왔다 갔던 구역 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 231117/왔다 갔던 구역 2/area-been-to-and-from2.swift
+++ 231117/왔다 갔던 구역 2/area-been-to-and-from2.swift
@@ -4,18 +4,16 @@ var graph = [Int](repeating: 0, count: 2001)
 var result = 0
 var sIndex: Int? = nil
 
-
 for _ in 0 ..< n {
     let raw = readLine()!.split { $0 == " " }
     let x = Int(raw[0])!
     
-    if raw[1] == "R" { for i in start ... start + x { graph[i] += 1 }; start += x }
-    else { for i in stride(from:start, through: start - x, by: -1) { graph[i] += 1 }; start -= x }
+    if raw[1] == "R" { for i in start ..< start + x { graph[i] += 1 }; start += x }
+    else { for i in stride(from:start - 1, through: start - x, by: -1) { graph[i] += 1 }; start -= x }
 }
 
 for i in 0 ... 2000 {
-    if graph[i] > 1 && sIndex == nil { sIndex = i }
-    if graph[i] < 2, let index = sIndex { sIndex = nil; result += abs(i - index) - 1 }
+    if graph[i] > 1 { result += 1 }
 }
 
 print(result)
