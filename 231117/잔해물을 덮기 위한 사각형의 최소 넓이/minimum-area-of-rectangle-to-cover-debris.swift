c0f1549707eab9ad0eeb4f846836731f7347d2f0
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-17 19:05:34 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 25ms, Memory: 4MB, Status: Passed - CodeTree

--- 231117/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.swift
+++ 231117/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.swift
@@ -1,43 +1,39 @@
 func draw(_ rect: [Int], _ isDraw: Bool) {
-    for i in min(rect[1], rect[3]) ..< max(rect[1], rect[3]) {
-        for j in min(rect[0], rect[2]) ..< max(rect[0], rect[2]) {
-            if isDraw { board[i][j] = true; count += 1 }
-            else { if board[i][j] { count -= 1 }; board[i][j] = false }
+    for i in rect[1] ..< rect[3] {
+        for j in rect[0] ..< rect[2] {
+            board[i][j] = isDraw
         }
     }
 }
 
 func check() {
-    var leftX = a[0]
-    var leftY = a[1]
-    var rightX = a[2]
-    var rightY = a[3]
+    var x1 = Int.max
+    var y1 = Int.max
+    var x2 = Int.min
+    var y2 = Int.min
 
-    for i in a[1] ..< a[3] {
-        for j in a[0] ..< a[2] {
-            
+    for i in a[1] ... a[3] {
+        for j in a[0] ... a[2] {
             guard board[i][j] else { continue }
             
-            if leftX > j { leftX = j }
-            if rightX < j { rightX = j }
-            if leftY > i { leftY = i }
-            if rightY < i { rightY = i }
-
+            x1 = min(x1, j)
+            y1 = min(y1, i)
+            x2 = max(x2, j)
+            y2 = max(y2, i)
+            found = true
         }
     }
 
-    print(abs(rightX - leftX) * abs(rightY - leftY))
+    if found { print((x2 - x1 + 1) * (y2 - y1 + 1)) }
+    else { print(0) }
 }
 
 let offSet = 1000
 let a = readLine()!.split { $0 == " " }.map { Int($0)! + offSet }
 let b = readLine()!.split { $0 == " " }.map { Int($0)! + offSet }
-var count = 0
+var found = false
 
 var board = [[Bool]](repeating: [Bool](repeating: false, count: 2001), count: 2001)
 draw(a, true)
 draw(b, false)
-
-
-if count == 0 { print(0) }
-else { check() }
+check()
