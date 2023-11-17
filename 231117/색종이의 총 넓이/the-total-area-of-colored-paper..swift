e4f4734f9295d2364bdc526679cb1a55a072c96a
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-17 16:30:46 +0900

    Title: 색종이의 총 넓이, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 231117/색종이의 총 넓이/the-total-area-of-colored-paper..swift
+++ 231117/색종이의 총 넓이/the-total-area-of-colored-paper..swift
@@ -7,7 +7,7 @@ for _ in 0 ..< n {
     let coordinate = readLine()!.split { $0 == " " }.map { Int($0)! + offSet }
     
     for i in coordinate[0] ..< coordinate[0] + 8 {
-        for j in coordinate[1] ..< coordinate[0] + 8 {
+        for j in coordinate[1] ..< coordinate[1] + 8 {
             if !board[i][j] { result += 1; board[i][j] = true }
         }
     }
