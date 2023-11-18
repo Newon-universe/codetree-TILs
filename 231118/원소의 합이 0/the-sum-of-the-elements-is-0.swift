8692aa8e365d000b93f053e8fbf84067368f5618
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-18 20:32:32 +0900

    Title: 원소의 합이 0, Time: 161ms, Memory: 21MB, Status: Passed - CodeTree

--- 231118/원소의 합이 0/the-sum-of-the-elements-is-0.swift
+++ 231118/원소의 합이 0/the-sum-of-the-elements-is-0.swift
@@ -1,21 +1,28 @@
-func find(_ depth: Int, _ now: Int) {
-    if depth == 4 {
-        if now == 0 { result += 1 }
-        return
-    }
-
-    for i in 0 ..< n {
-        find(depth + 1, now + board[depth][i])
-    }
-}
-
 let n = Int(readLine()!)!
 var result = 0
-
 var board = [[Int]]()
+
+var dictA = [Int:Int]()
+var dictB = [Int:Int]()
+
 for _ in 0 ..< 4 {
     board.append(readLine()!.split { $0 == " " }.map { Int($0)! })
 }
 
-find(0, 0)
+for i in 0 ..< n {
+    for j in 0 ..< n {
+        let value1 = board[0][i] + board[1][j]
+        let value2 = board[2][i] + board[3][j]
+
+        dictA[value1, default: 0] += 1
+        dictB[value2, default: 0] += 1
+    }
+}
+
+for aKey in dictA.keys {
+    if dictB[-aKey] != nil {
+        result += dictA[aKey, default: 0] * dictB[-aKey, default: 0]
+    }
+}
+
 print(result)
