809c4c5bd5aa7c5b3484b2827e8f5edf3cb81b67
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-20 22:39:07 +0900

    Title: 겹치지 않게 선분 고르기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231120/겹치지 않게 선분 고르기/select-segments-without-overlap.swift
@@ -0,0 +1,36 @@
+func duplicatedCheck(_ a: [Int], _ b: [Int]) -> Bool {
+    if a[0] ... a[1] ~= b[0] || a[0] ... a[1] ~= b[1] { return true }
+    return false
+}
+
+func checker(_ depth: Int, _ now: [[Int]], _ index: Int) {
+    if depth == n {
+        return
+    }
+
+    result = max(now.count, result)
+
+    for i in index ..< lines.count {
+
+        var isDuplicated = false
+        for line in now {
+            isDuplicated = duplicatedCheck(line, lines[i])
+            if isDuplicated { break }
+        }
+        guard !isDuplicated else { continue }
+
+        checker(depth + 1, now + [lines[i]], index + 1)
+    }
+}
+
+let n = Int(readLine()!)!
+var lines = [[Int]]()
+var visited = [Bool](repeating: false, count: n)
+var result = 0
+
+for _ in 0 ..< n {
+    lines.append(readLine()!.split { $0 == " " }.map { Int($0)! })
+}
+
+checker(0, [], 0)
+print(result)
