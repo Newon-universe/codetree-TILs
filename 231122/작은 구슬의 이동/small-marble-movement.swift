543cbb44e7a1282650fc6d2a6a0fd42475e3744f
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-22 18:22:55 +0900

    Title: 작은 구슬의 이동, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 231122/작은 구슬의 이동/small-marble-movement.swift
+++ 231122/작은 구슬의 이동/small-marble-movement.swift
@@ -16,19 +16,19 @@ let raw = readLine()!.split { $0 == " " }.map { String($0) }
 var now = Coordinate(Int(raw[0])!, Int(raw[1])!, raw[2])
 
 
-for _ in 0 ..< t {
+for i in 0 ..< t {
     switch now.direct {
     case "L": 
         guard now.x - 1 > 0 else { now.direct = "R"; break }
         now.x -= 1
     case "R":
-        guard now.x + 1 < n else { now.direct = "L"; break }
+        guard now.x + 1 <= n else { now.direct = "L"; break }
         now.x += 1
     case "U":
         guard now.y - 1 > 0 else { now.direct = "D"; break }
         now.y -= 1
     case "D":
-        guard now.y + 1 < n else { now.direct = "U"; break }
+        guard now.y + 1 <= n else { now.direct = "U"; break }
         now.y += 1
     default: break
     }
