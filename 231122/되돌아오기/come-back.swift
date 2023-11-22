cecaa904928bb9b7fd5c9d41670d5e6f28e7ab67
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-22 19:34:20 +0900

    Title: 되돌아오기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231122/되돌아오기/come-back.swift
@@ -0,0 +1,52 @@
+struct Coordinate {
+    var y: Int
+    var x: Int
+    init(_ y: Int, _ x: Int) {
+        self.y = y
+        self.x = x
+    }
+}
+
+let n = Int(readLine()!)!
+var oper = [[String]]()
+var now = Coordinate(0, 0)
+var result = 0
+
+for _ in 0 ..< n {
+    let raw = readLine()!.split { $0 == " " }.map { String($0) }
+    oper.append(raw)
+}
+
+for value in oper {
+    switch value[0] {
+    case "N": 
+        for _ in 0 ..< Int(value[1])! {
+            now.y -= 1
+            result += 1
+            if now.y == 0 && now.x == 0 { break }
+        }
+    case "S": 
+        for _ in 0 ..< Int(value[1])! {
+            now.y += 1
+            result += 1
+            if now.y == 0 && now.x == 0 { break }
+        }
+    case "E": 
+        for _ in 0 ..< Int(value[1])! {
+            now.x += 1
+            result += 1
+            if now.y == 0 && now.x == 0 { break }
+        }
+    case "W": 
+        for _ in 0 ..< Int(value[1])! {
+            now.x -= 1
+            result += 1
+            if now.y == 0 && now.x == 0 { break }
+        }
+    default: break
+    }
+    
+    if now.y == 0 && now.x == 0 { break }
+}
+
+print(result)
