29a5790d0fd37b60c81ade61dba56d6e18dd8fec
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-23 16:29:21 +0900

    Title: 그래프 탐색, Time: 25ms, Memory: 1MB, Status: Passed - CodeTree

--- 231123/그래프 탐색/graph-traversal.swift
+++ 231123/그래프 탐색/graph-traversal.swift
@@ -1,21 +1,19 @@
-func dfs(_ depth: Int, _ now: Int) -> Int {
-    var result = depth
-
+func dfs(_ depth: Int, _ now: Int) {
+    
     for j in 0 ..< n {
         guard board[now][j] else { continue }
         guard !visited[j] else { continue }
 
         visited[j] = true
-        result = max(dfs(depth + 1, j), result)
-        visited[j] = false
+        dfs(depth + 1, j)
     }
 
-    return result
 }
 
 let source = readLine()!.split { $0 == " " }.map { Int($0)! }
 let (n, m) = (source[0], source[1])
 var board = [[Bool]](repeating: [Bool](repeating: false, count: n), count: n)
+var result = Set<Int>()
 
 for _ in 0 ..< m {
     let raw = readLine()!.split { $0 == " " }.map { Int($0)! - 1 }
@@ -26,5 +24,5 @@ for _ in 0 ..< m {
 
 var visited = [Bool](repeating: false, count: n)
 visited[0] = true
-
-print(dfs(0, 0))
+dfs(0, 0)
+print(visited.filter { $0 == true }.count - 1)
