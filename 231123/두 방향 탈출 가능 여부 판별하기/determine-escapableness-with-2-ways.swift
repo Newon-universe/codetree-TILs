a2d7bceaee6eb57255473b86e6288497cb6a55b4
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-23 17:54:24 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231123/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.swift
@@ -0,0 +1,40 @@
+struct Coordinate {
+    var y: Int
+    var x: Int
+    init(_ y: Int, _ x: Int) {
+        self.y = y
+        self.x = x
+    }
+}
+
+let directions = [
+    Coordinate(1, 0),
+    Coordinate(0, 1)
+]
+
+func dfs(_ now: Coordinate) {
+    if now.y == n - 1 && now.x == n - 1 {
+        result = 1
+        return
+    }
+
+    for direct in directions {
+        let next = Coordinate(now.y + direct.y, now.x + direct.x)
+        
+        guard 0 ..< n ~= next.y && 0 ..< m ~= next.x else { continue }
+        guard board[next.y][next.x] == 1 else { continue }
+        dfs(next)
+    }
+}
+
+let raw = readLine()!.split { $0 == " " }.map { Int($0)! }
+let (n, m) = (raw[0], raw[1])
+var result = 0
+
+var board = [[Int]]()
+for _ in 0 ..< n {
+    board.append(readLine()!.split { $0 == " " }.map { Int($0)! })
+}
+
+dfs(Coordinate(0 ,0))
+print(result)
