c8e3507bd3d501282d4778cf309f24dd24edc53f
Newon <80164141+Newon-universe@users.noreply.github.com>
2023-11-25 11:32:03 +0900

    Title: 최단 거리 11, Time: 191ms, Memory: 4MB, Status: Passed - CodeTree

---
+++ 231125/최단 거리 11/shortest-distance-11.swift
@@ -0,0 +1,131 @@
+struct PriorityQueue<T: Equatable> {
+    private var elements: [T]
+    private let sort: (T, T) -> Bool
+
+    init(_ sort: @escaping (T, T) -> Bool,_ elements: [T] = []) {
+        self.sort = sort
+        self.elements = elements
+
+        if !elements.isEmpty {
+            for i in stride(from: (elements.count / 2) - 1, to: -1, by: -1) { siftDown(i) }
+        }
+    }
+
+    var isEmpty: Bool { elements.isEmpty }
+    var count: Int { elements.count }
+
+    private func leftChildIndex(_ i: Int) -> Int { return (2 * i) + 1 }
+    private func rightChildIndex(_ i: Int) -> Int { return (2 * i) + 2 }
+    private func parentIndex(_ i: Int) -> Int { return (i - 1) / 2 }
+
+    mutating func enqueue(_ node: T) {
+        elements.append(node)
+        siftUp(count - 1)
+    }
+
+    mutating private func siftUp(_ i: Int) {
+        var child = i
+        var parent = parentIndex(child)
+
+        while child > 0 && sort(elements[child], elements[parent]) {
+            elements.swapAt(child, parent)
+            child = parent
+            parent = parentIndex(child)
+        }
+    }
+
+    mutating func dequeue() -> T? {
+        guard !isEmpty else { return nil }
+
+        elements.swapAt(0, count - 1)
+        defer { siftDown(0) }
+        return elements.removeLast()
+    }
+
+    mutating private func siftDown(_ i: Int) {
+        var parent = i
+        while true {
+            let left = leftChildIndex(parent)
+            let right = rightChildIndex(parent)
+            var candidate = parent
+
+            if left < count && sort(elements[left], elements[candidate]) { candidate = left }
+            if right < count && sort(elements[right], elements[candidate]) { candidate = right }
+            if parent == candidate { return }
+
+            elements.swapAt(candidate, parent)
+            parent = candidate
+        }
+    }
+}
+
+struct Node: Equatable {
+    let vertex: Int
+    let dist: Int
+    init(_ vertex: Int, _ dist: Int) {
+        self.vertex = vertex
+        self.dist = dist
+    }
+}
+
+func solve() {
+    var pq = PriorityQueue<Node> { $0.dist < $1.dist }
+    var distances = [Int:Int]()
+    var visited = [Int:Bool]()
+
+    pq.enqueue(Node(a, 0))
+    for key in graph.keys {
+        let value = key == a ? 0 : Int.max
+        distances.updateValue(value, forKey: key)
+        visited.updateValue(false, forKey: key)
+    }
+
+    while let now = pq.dequeue() {
+        guard !visited[now.vertex]! else { continue }
+        visited[now.vertex] = true
+
+        for (vertex, dist) in graph[now.vertex]! {
+            let newDist = now.dist + dist
+            if newDist < distances[vertex]! {
+                distances[vertex] = newDist
+                pq.enqueue(Node(vertex, newDist))
+            }
+        }
+    }
+
+    let result = "\(distances[b]!)\n"
+    var tempIndex = b
+    var temp = "\(b) "
+
+    while tempIndex != a {
+        for i in stride(from: n, to: 0, by: -1) { 
+            guard let edge = graph[i] else { continue }
+            guard let edgeDist = edge[tempIndex] else { continue }
+            guard let currentDist = distances[i] else { continue }
+
+            if currentDist + edgeDist == distances[tempIndex]! {
+                tempIndex = i
+                temp = "\(i) " + temp
+                break
+            }
+        }
+    }
+
+    print(result + temp)
+}
+
+
+let source = readLine()!.split { $0 == " " }.map { Int($0)! }
+let (n, m) = (source[0], source[1])
+
+var graph = [Int: [Int: Int]]()
+for _ in 0 ..< m {
+    let raw = readLine()!.split { $0 == " " }.map { Int($0)!}
+    graph[raw[0], default: [:]].merge([raw[1]: raw[2]]) { $1 }
+    graph[raw[1], default: [:]].merge([raw[0]: raw[2]]) { $1 }
+}
+
+let source2 = readLine()!.split { $0 == " " }.map { Int($0)! }
+let (a, b) = (source2[0], source2[1])
+
+solve()
